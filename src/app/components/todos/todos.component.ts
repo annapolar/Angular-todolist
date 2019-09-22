00b3b38741d1b5cdfa06f000b31cccb330bed521
Anna Huang <annatointro@gmail.com>
2019-09-22 02:42:29 -0600

    ngFor todo items

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todos';
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
+  todos:Todo[];
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {   // same as create in Vue.js
+    this.todos = [
+      {
+        id: 1,
+        title: 'Todo one',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Todo two',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Todo three',
+        completed: false
+      }
+    ]
   }
-
 }
